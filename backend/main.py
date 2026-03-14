b41952bf432f8e48cb544a0920d8be4f620ac8c5
Boong <heangboongtaing@gmail.com>
2026-03-14 10:48:51 +0700

    add allow origin

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://condopricepredictionbackend.vercel.app",
+        "https://condopricepredictionfrontend.vercel.app",
         "http://localhost:5500",
         "http://127.0.0.1:5500"
     ],
